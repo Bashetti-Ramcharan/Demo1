e929db618279de4ba35b195e6189559614792532
Bashetti-Ramcharan <87516202+Bashetti-Ramcharan@users.noreply.github.com>
2022-10-31 10:00:31 +0530

    Added exclamations

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -8,8 +8,8 @@ public class Demo1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
-        System.out.println("Hello World");
-        System.out.println("hii");
+        System.out.println("Hello World!!");
+        System.out.println("hii!!");
     }
 
 }
